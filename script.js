ed8d12eaf064a3a32e0f131e9b96b0204c7e50f3
D0708054 <56072390+D0708054@users.noreply.github.com>
2024-09-18 02:23:46 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+document.getElementById('contact-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('Form submitted!');
+});
+
+document.getElementById('vote-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('Thank you for voting!');
+});
